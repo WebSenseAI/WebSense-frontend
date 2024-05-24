f08adf4150be7bd3b2d3650fa3f9dde2505f3f62
Luis Beqja <luisbeqjamw@gmail.com>
2024-05-24 17:38:04 +0200

    add github login

--- src/router.ts
+++ src/router.ts
@@ -3,14 +3,14 @@ import { createWebHistory, createRouter } from 'vue-router'
 import HomeView from './views/home.vue'
 import SetupView from './views/setup.vue'
 import DemoView from './views/demo.vue'
-import GoogleLogin from './views/google_login.vue'
+import LoginButtons from './views/login_buttons.vue'
 import LoadingBot from './views/loading_bot.vue'
 
 const routes = [
     { path: '/', component: HomeView },
     { path: '/setup', component: SetupView },
     { path: '/demo', component: DemoView },
-    { path: '/login', component: GoogleLogin },
+    { path: '/login', component: LoginButtons },
     { path: '/loadingbot', component: LoadingBot },
 ]
 
