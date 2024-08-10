1a862559f658d838d5579f09f8468236b7ed35f7
luis beqja <luisbeqjamw@gmail.com>
2024-08-10 16:06:46 +0200

    fix auth with access token

--- src/components/common/LoginButtons.vue
+++ src/components/common/LoginButtons.vue
@@ -31,12 +31,12 @@
 import { SERVER_URL } from '@/utils/url';
 import { useAxios } from '@/composables/axios';
 
-const { post } = useAxios(SERVER_URL);
+const { get, data } = useAxios(SERVER_URL);
 
 const loginWithService = async (service: string) => {
-  await post(`${SERVER_URL}/auth/register/oauth/${service}`, {});
-/*   const { url } = data.value;
-  window.location.href = url; */
+  await get(`${SERVER_URL}/auth/register/oauth/${service}`);
+  const { url } = data.value;
+  window.location.href = url;
 };
 </script>
 
