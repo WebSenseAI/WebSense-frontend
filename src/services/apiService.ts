3fdacf61d057226a45ef397c88d2c0948508cb2b
Tan Dalkıran <tan.dalkiran@gmail.com>
2024-08-24 18:05:39 +0300

    fix: fixed async headers error

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -2,20 +2,25 @@ import { supabase } from "./supabaseService";
 import { SERVER_URL } from '@/utils/url';
 import axios, { AxiosRequestConfig } from "axios";
 
-const session = (await supabase.auth.getSession()).data.session;
 
-const headers: AxiosRequestConfig = {
-    'headers' : {
-        'Content-Type' : 'application/json',
-        'Authorization' : `Bearer ${session?.access_token}`
-    },
-    
+const getHeaders = async () => {
+    const session = (await supabase.auth.getSession()).data.session;
+    const headers: AxiosRequestConfig = {
+        'headers' : {
+            'Content-Type' : 'application/json',
+            'Authorization' : `Bearer ${session?.access_token}`
+        },
+        
+    }
+    return headers;
 }
+
+
 
 export async function postServer(url:any, post_data:any){
     const URL = `${SERVER_URL}${url}`    
     try {
-        const response = await axios.post(URL, post_data, headers);
+        const response = await axios.post(URL, post_data, await getHeaders());
         return response;
     } catch (error) {
         console.error('Error occured on API service', error);
@@ -26,7 +31,7 @@ export async function postServer(url:any, post_data:any){
 export async function getServer(url:any){
     const URL = `${SERVER_URL}${url}`
     try {
-        const response = await axios.get(URL, headers);
+        const response = await axios.get(URL, await getHeaders());
         return response;
     } catch (error) {
         console.error('Error occured on API service', error);
@@ -37,7 +42,7 @@ export async function getServer(url:any){
 export async function removeServer(url:any) {
     const URL = `${SERVER_URL}${url}`
     try {
-        const response = await axios.delete(URL, headers)
+        const response = await axios.delete(URL, await getHeaders())
         return response;
     } catch (error) {
         console.error('Error occured on API service', error);
@@ -49,7 +54,7 @@ export async function removeServer(url:any) {
 export async function putServer(url: any, data: any) {
     const URL = `${SERVER_URL}${url}`
     try {
-        const response = await axios.put(URL, data, headers);
+        const response = await axios.put(URL, data, await getHeaders());
         return response;
     } catch (error) {
         console.error('Error occured on API service', error);
